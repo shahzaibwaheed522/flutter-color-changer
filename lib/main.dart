7d03b73bc7559e752a7b700eda2f26893ad8cfeb
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-07 16:29:42 +0500

    Container with Star

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-   debugShowCheckedModeBanner: false,
-   home:MyApp(),
+    debugShowCheckedModeBanner: false,
+    home:MyApp(),
   ));
 }
 class MyApp extends StatelessWidget{
@@ -11,31 +11,29 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Stack 2 Example"),
-        backgroundColor: Colors.green,
+        title: const Text("Container with star example"),
       ),
       body: Center(
         child: Stack(
           children: [
-            Image.network(
-              "https://picsum.photos/300/200",
-              width: 300,
-              height: 200,
-              fit:BoxFit.cover,
+            Container(
+              width: 250,
+              height: 250,
+              color:Colors.blue,
             ),
-            const Text(
-              "Beautiful Nature",
-              style: TextStyle(
-                color:Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
+            const Positioned(
+              top:20,
+              left:20,
+              child: Icon(
+                Icons.stars,
+                size:60,
+                color:Colors.yellow,
             ),
-            
-
+            ),
           ],
         ),
       ),
+      
     );
   }
 }
