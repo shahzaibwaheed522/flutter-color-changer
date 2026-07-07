ed0a39ba8740e2c52c062f0d52f8567597e55782
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-07 16:55:09 +0500

    Bottom Right Icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,38 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:MyApp(),
+  home:WelcomePage(),
   ));
 }
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-  @override  
+class WelcomePage extends StatelessWidget{
+  const WelcomePage({super.key});
+  @override   
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Container with star example"),
+        title: const Text("Another Example"),
+        backgroundColor: Colors.green,
       ),
       body: Center(
         child: Stack(
           children: [
-            Container(
-              width: 250,
+             Container(
+              width:250,
               height: 250,
               color:Colors.blue,
-            ),
-            const Positioned(
-              top:20,
-              left:20,
+             ),
+             const Positioned(
+              bottom:10,
+              right:10,
               child: Icon(
-                Icons.stars,
-                size:60,
-                color:Colors.yellow,
-            ),
-            ),
+                Icons.home,
+                size:40,color: Colors.white,
+              ),
+              ),
           ],
         ),
       ),
-      
     );
   }
+
 }
