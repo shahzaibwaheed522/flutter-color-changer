5b043045631202ea1818e5cf761b38499e6fee2f
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-07 15:46:27 +0500

    add image + text stack UI using Flutter Stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,37 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-  home:LoginPage(),
+   debugShowCheckedModeBanner: false,
+   home:MyApp(),
   ));
 }
-class LoginPage extends StatelessWidget{
-  const LoginPage({super.key});
-  @override   
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override  
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login Page"),
+        title: const Text("Stack 2 Example"),
+        backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: Stack(
           children: [
-            const Icon(
-              Icons.person,
-              size: 80,
-              color: Colors.greenAccent,
+            Image.network(
+              "https://picsum.photos/300/200",
+              width: 300,
+              height: 200,
+              fit:BoxFit.cover,
             ),
             const Text(
-               "Login",
-               style: TextStyle(
-                fontSize: 30,
+              "Beautiful Nature",
+              style: TextStyle(
+                color:Colors.white,
+                fontSize: 28,
                 fontWeight: FontWeight.bold,
-               ),
-            ),
-            const SizedBox(height:20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height:20),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-             SizedBox(
-              child: ElevatedButton(onPressed: (){}, child: const Text("Dont have a account Please Signup?")),
-             )
+            
 
           ],
         ),
