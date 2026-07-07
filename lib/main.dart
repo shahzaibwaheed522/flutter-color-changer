2de1ff144d3a841aaba422d9b2371b698f8080a9
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-07 18:00:56 +0500

    Notification Badge

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,50 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-  home:WelcomePage(),
+   home:MyApp(),
   ));
 }
-class WelcomePage extends StatelessWidget{
-  const WelcomePage({super.key});
-  @override   
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override  
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Another Example"),
+        title: const Text("Notification Badge"),
         backgroundColor: Colors.green,
       ),
       body: Center(
         child: Stack(
           children: [
-             Container(
-              width:250,
-              height: 250,
-              color:Colors.blue,
-             ),
-             const Positioned(
-              bottom:10,
-              right:10,
-              child: Icon(
-                Icons.home,
-                size:40,color: Colors.white,
+            const Icon(
+              Icons.shopping_cart,
+              size:100,
+              color:Colors.black,
+            ),
+            Positioned(   
+              top:0,
+              right: 0,
+              child: Container(
+                width:30,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+                child: const Center(
+                  child: Text(
+                    "3",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
-              ),
+            )
           ],
         ),
       ),
     );
   }
-
 }
