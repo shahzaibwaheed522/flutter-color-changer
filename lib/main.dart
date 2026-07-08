7a9dbbe8f13140f6eeebe50b81dd8f9da2ec12bd
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-08 16:22:30 +0500

    List of Colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,18 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-   home:MyApp(),
+  home:Scaffold(
+    appBar: AppBar(
+      title: const Text("List View"),
+    ),
+    body: ListView(
+      children: [
+        Container(height: 100,color:Colors.red),
+        Container(height: 100,color:Colors.yellow),
+        Container(height: 100,color:Colors.pink),
+        Container(height: 100,color:Colors.green),
+      ],
+    ),
+  ),
   ));
-}
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-  @override  
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Notification Badge"),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            const Icon(
-              Icons.shopping_cart,
-              size:100,
-              color:Colors.black,
-            ),
-            Positioned(   
-              top:0,
-              right: 0,
-              child: Container(
-                width:30,
-                height: 30,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
-                ),
-                child: const Center(
-                  child: Text(
-                    "3",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
