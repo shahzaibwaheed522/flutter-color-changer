e93f73ba5daf4bccb4f9cf1c72679a5a12ab068e
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-08 21:07:18 +0500

    Grid-View

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,19 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Example"),
-      ),
-      body:ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title:Text("Shahzaib"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Bilal"),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Ahmed"),
-          ),
-        ],
-      )
+   home:Scaffold(
+    appBar: AppBar(
+      title: const Text("Grid View Simple Exmaple"),
     ),
+    body:GridView.count(crossAxisCount: 
+    2,
+    children: [
+      Container(color:Colors.red),
+      Container(color: Colors.yellow),
+      Container(color:Colors.pink),
+      Container(color: Colors.green),
+    ],
+    ),
+   ),
   ));
 }
