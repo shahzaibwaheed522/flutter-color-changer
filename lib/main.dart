5ac668de2e637211f5bf56579a6dcc93aa9ed8e3
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-09 16:54:30 +0500

    BMI-CALCULATOR

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 void main(){
   runApp(MaterialApp(
- home:FirstScreen(),
+  debugShowCheckedModeBanner: false,
+  home:MyApp(),
   ));
 }
-class FirstScreen extends StatelessWidget{
-  const FirstScreen({super.key});
+class MyApp extends StatefulWidget{
+   const MyApp({super.key});
+   State<MyApp> createState()=> _MyAppState();
+}
+class _MyAppState extends State<MyApp>{
+  TextEditingController weightcontroller=TextEditingController();
+  TextEditingController heightcontroller=TextEditingController();
+  String result="";
+  String category="";
+  void calculate(){
+    double weight=double.parse(weightcontroller.text);
+    double height=double.parse(heightcontroller.text);
+    double bmi=weight/(height*height);
+    setState(() {
+      result=bmi.toStringAsFixed(2);
+      if(bmi<18.5){
+        category="Normal Weight";
+      }else if(bmi>18.5&&bmi<25){
+        category="Underweight";
+      }
+      else if(bmi>25&&bmi<30){
+        category="Overweight";
+      }else{
+        category="Obese";
+      }
+      });
+  }
   @override  
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("First Screen"),
-
+        title: const Text("BMI CALCULATOR"),
       ),
       body: Center(
-        child: ElevatedButton( onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: 
-          (context)=>SecondScreen(),
-          ),
-          );
-        },
-        child: Text("Go next"),
-        ),
-      ),
-    );
-  }
-}
-class SecondScreen extends StatelessWidget{
-  const SecondScreen({super.key});
-  @override   
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Screen"),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: (){
-          Navigator.pop(context);
-        }, child: const Text("Go to the first Screen"),
-        
+        child: Column(
+          children: [
+            TextField(
+              controller: weightcontroller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText:"Enter your weight (kg)",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height:20),
+            TextField(
+              controller:heightcontroller,
+              keyboardType: TextInputType.number,
+              decoration:InputDecoration(
+                labelText:"Enter your height (m)",
+                hintText: "Example:1.75",
+                border:OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              width:double.infinity,
+              child:ElevatedButton(onPressed: calculate, child: const Text("BMI CALCULATE")
+              ),
+            ),
+            SizedBox(height:30),
+            Text(
+              "BMI :$result",
+              style: TextStyle(
+                fontSize: 24,
+                color:Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              category,
+              style:TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
