82dbfd312fc6dab1f0d0ddae27aa6bb1b2cbef86
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-13 19:15:58 +0500

    Form Validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,65 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home:MyApp(),
+   debugShowCheckedModeBanner: false,
+   home:FormScreen(),
   ));
 }
-class MyApp extends StatefulWidget{
-   const MyApp({super.key});
-   State<MyApp> createState()=> _MyAppState();
+class FormScreen extends StatefulWidget{
+  const FormScreen({super.key});
+  State<FormScreen> createState()=> _formScreenState();
 }
-class _MyAppState extends State<MyApp>{
-  TextEditingController weightcontroller=TextEditingController();
-  TextEditingController heightcontroller=TextEditingController();
-  String result="";
-  String category="";
-  void calculate(){
-    double weight=double.parse(weightcontroller.text);
-    double height=double.parse(heightcontroller.text);
-    double bmi=weight/(height*height);
-    setState(() {
-      result=bmi.toStringAsFixed(2);
-      if(bmi<18.5){
-        category="Normal Weight";
-      }else if(bmi>18.5&&bmi<25){
-        category="Underweight";
-      }
-      else if(bmi>25&&bmi<30){
-        category="Overweight";
-      }else{
-        category="Obese";
-      }
-      });
-  }
-  @override  
+class _formScreenState extends State<FormScreen>{
+  final _formkey=GlobalKey<FormState>();
+  TextEditingController namecontroller=TextEditingController();
+  TextEditingController emailcontroller=TextEditingController();
+  @override   
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("BMI CALCULATOR"),
+        title: const Text("Form Validation"),
       ),
-      body: Center(
+      body:Padding(   
+        padding:EdgeInsets.all(20),
         child: Column(
           children: [
-            TextField(
-              controller: weightcontroller,
-              keyboardType: TextInputType.number,
+            TextFormField(
+              controller: namecontroller,
               decoration: InputDecoration(
-                labelText:"Enter your weight (kg)",
+                labelText: "Enter the Name",
                 border: OutlineInputBorder(),
               ),
+              validator: (value){
+                if(value!.isEmpty){
+                  return "Name";
+                }
+                return null;
+              },
             ),
-            SizedBox(height:20),
-            TextField(
-              controller:heightcontroller,
-              keyboardType: TextInputType.number,
-              decoration:InputDecoration(
-                labelText:"Enter your height (m)",
-                hintText: "Example:1.75",
-                border:OutlineInputBorder(),
+            TextFormField(
+              controller: emailcontroller,
+              decoration: InputDecoration(
+                labelText:"Email",
+                border: OutlineInputBorder(),
               ),
+              validator: (value){
+                if(value!.isEmpty){
+                  return "Email";
+                }
+                return null;
+              },
             ),
-            SizedBox(
-              width:double.infinity,
-              child:ElevatedButton(onPressed: calculate, child: const Text("BMI CALCULATE")
-              ),
-            ),
-            SizedBox(height:30),
-            Text(
-              "BMI :$result",
-              style: TextStyle(
-                fontSize: 24,
-                color:Colors.blue,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              category,
-              style:TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: (){
+              if(_formkey.currentState!.validate()){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Welcome  ${namecontroller.text}",),),
+                
+                );
+                print(namecontroller.text);
+                print(emailcontroller.text);
+              }
+            }, child: const Text("Submit"),),
           ],
         ),
       ),
