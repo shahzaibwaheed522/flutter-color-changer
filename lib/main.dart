cc5f41ad521a9dc3309b82647b42833c6ed82a9f
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-14 13:10:07 +0500

    Password Validator

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,56 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-   debugShowCheckedModeBanner: false,
-   home:FormScreen(),
+    debugShowCheckedModeBanner: false,
+   home:PasswordValidator(),
   ));
 }
-class FormScreen extends StatefulWidget{
-  const FormScreen({super.key});
-  State<FormScreen> createState()=> _formScreenState();
+class PasswordValidator extends StatefulWidget{
+  const PasswordValidator({super.key});
+  State<PasswordValidator> createState()=> _PasswordValidatorState();
 }
-class _formScreenState extends State<FormScreen>{
-  final _formkey=GlobalKey<FormState>();
-  TextEditingController namecontroller=TextEditingController();
-  TextEditingController emailcontroller=TextEditingController();
-  @override   
-  Widget build(BuildContext context){
+class _PasswordValidatorState extends State<PasswordValidator>{
+   final _formkey= GlobalKey<FormState>();
+   TextEditingController passwordcontroller= TextEditingController();
+   @override   
+   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Form Validation"),
+        title: const Text("Password Validator"),
       ),
-      body:Padding(   
-        padding:EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: namecontroller,
-              decoration: InputDecoration(
-                labelText: "Enter the Name",
-                border: OutlineInputBorder(),
+      body:Padding (    
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller:passwordcontroller,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+                validator:(value){
+                  if(value!.isEmpty && value.length<6){
+                    return "Enter Password or password must be at least 6 character";
+                  }     
+                  return null;
+                },
               ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Name";
+              const SizedBox(height:10),
+              ElevatedButton(onPressed: (){
+                if(_formkey.currentState!.validate()){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Password Accepted"),),
+                  );
+                  print(passwordcontroller.text);
                 }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: emailcontroller,
-              decoration: InputDecoration(
-                labelText:"Email",
-                border: OutlineInputBorder(),
-              ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return "Email";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(onPressed: (){
-              if(_formkey.currentState!.validate()){
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Welcome  ${namecontroller.text}",),),
-                
-                );
-                print(namecontroller.text);
-                print(emailcontroller.text);
-              }
-            }, child: const Text("Submit"),),
-          ],
+              }, child: const Text("Submit"),),
+            ],
+          ),
         ),
       ),
     );
-  }
+   }
 }
