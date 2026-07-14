ca69b7b265a5ffdb63dc608a923d2b790fb1f5a7
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-14 15:33:34 +0500

    Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,32 @@
 import 'package:flutter/material.dart';
+
 void main(){
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-   home:PasswordValidator(),
+  debugShowCheckedModeBanner: false,
+  home:HomeScreen(),
   ));
 }
-class PasswordValidator extends StatefulWidget{
-  const PasswordValidator({super.key});
-  State<PasswordValidator> createState()=> _PasswordValidatorState();
-}
-class _PasswordValidatorState extends State<PasswordValidator>{
-   final _formkey= GlobalKey<FormState>();
-   TextEditingController passwordcontroller= TextEditingController();
-   @override   
-   Widget build(BuildContext context){
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+  @override   
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Password Validator"),
+        title: const Text("Custom Widget"),
       ),
-      body:Padding (    
-        padding: EdgeInsets.all(20),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller:passwordcontroller,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(),
-                ),
-                validator:(value){
-                  if(value!.isEmpty && value.length<6){
-                    return "Enter Password or password must be at least 6 character";
-                  }     
-                  return null;
-                },
-              ),
-              const SizedBox(height:10),
-              ElevatedButton(onPressed: (){
-                if(_formkey.currentState!.validate()){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text("Password Accepted"),),
-                  );
-                  print(passwordcontroller.text);
-                }
-              }, child: const Text("Submit"),),
-            ],
-          ),
+      body:Center(
+        child: MyButton(
+          title:"Login",
         ),
       ),
     );
-   }
+  }
+}
+class MyButton extends StatelessWidget{
+  final String title;
+  const MyButton({super.key, required this.title});
+  @override   
+  Widget build(BuildContext context){
+    return ElevatedButton(onPressed: (){}, child: Text(title),);
+  }
 }
