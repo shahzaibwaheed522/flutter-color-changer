e0e5addbd80b6f58455ee6e7aa2f6c106a9d79a6
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-15 21:41:47 +0500

    Bottom Naigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 void main(){
-   runApp(const MyApp());
+  runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home:Screen(),
+  ));
 }
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-  @override   
-  Widget build(BuildContext context){
-    return MaterialApp(
-    theme: ThemeData.light(),
-    darkTheme: ThemeData.dark(),
-    themeMode: ThemeMode.dark,
-      home:const HomeScreen(),
-    );
-  }
+class Screen extends StatefulWidget{
+  const Screen({super.key});
+  State<Screen> createState()=> _ScreenState();
 }
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({super.key});
+class _ScreenState extends State<Screen>{
+  int currentindex=0;
+  List <Widget> screens=const[
+  Center(child: Text("Home Screen",style: TextStyle(fontSize: 25))),
+  Center(child: Text("SearchScreen",style: TextStyle(fontSize: 25))),
+  Center(child: Text("Profile Scree",style: TextStyle(fontSize: 25))),
+  ];
   @override   
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Theme"),
+        title: const Text("Bottom Navigation Bar"),
       ),
-      body:Center(
-        child:Text(
-          "Hello Shahzaib",
-          style: TextStyle(fontSize:20),
-        ),
-      ),
+      body:screens[currentindex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentindex,
+        onTap: (index){
+          setState(() {
+            currentindex=index;
+          });
+        },
+        items:const[
+          BottomNavigationBarItem(icon: Icon(Icons.home),
+            label:"Home",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.search),
+          label:"Search",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label:"Profile"),
+        ],),
     );
   }
 }
