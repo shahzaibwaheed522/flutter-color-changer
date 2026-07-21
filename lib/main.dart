6951405fc82c6d0294b3e507a47b63519060a926
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-21 17:40:48 +0500

    Add SharedPreference with highscore persistence

--- lib/main.dart
+++ lib/main.dart
@@ -2,76 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 void main(){
   runApp(MaterialApp(
-   debugShowCheckedModeBanner: false,
-   home:HomeScreen(),
+  debugShowCheckedModeBanner: false,
+   home:ScoreScreen(),
   ));
 }
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({super.key});
-  State<HomeScreen> createState()=> HomeScreenState();
+class ScoreScreen extends StatefulWidget{
+  const ScoreScreen({super.key});
+  State<ScoreScreen> createState()=> _ScoreScreenState();
 }
-class HomeScreenState extends State<HomeScreen>{
-  TextEditingController namecontroller=TextEditingController();
-  String username="";
-  Future<void> savedate() async{
-    SharedPreferences prefs= await SharedPreferences.getInstance();
-    prefs.setString("Username", namecontroller.text);
+class _ScoreScreenState extends State<ScoreScreen>{
+  TextEditingController scorecontroller=TextEditingController();
+  int highscore=0;
+  Future<void> saveScore() async{
+    SharedPreferences prefs=await SharedPreferences.getInstance();
+    await prefs.setInt("Highscore", int.parse(scorecontroller.text),);
+
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Username Saved"),),
+      SnackBar(content: Text("Score-Saved"),),
     );
+     print(prefs.getInt("highScore"));
   }
-  Future<void>readdate() async{
+  Future<void> readScore() async{
     SharedPreferences prefs= await SharedPreferences.getInstance();
+    print("Read: ${prefs.getInt("highScore")}");
     setState(() {
-    username=prefs.getString("username")?? "No Data";
-       });
+      highscore=prefs.getInt("highscore")??0;
+    });
   }
   @override   
   void initState(){
     super.initState();
-    readdate();
+    readScore();
   }
+  @override  
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Shared Preference"),
+        title: const Text("High-Score"),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.cyanAccent,
       ),
-      body:Padding(padding: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          TextField(
-            controller: namecontroller,
-            decoration: InputDecoration(
-              labelText: "Enter the username",
-              border:OutlineInputBorder(),
+      body:Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller:scorecontroller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText:"Enter Score",
+                border:OutlineInputBorder(),
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(onPressed: ()async
-          {
-             await savedate();
-             readdate();
-
-          }, child: Text("Saved"),),
-
-          SizedBox(height: 20),
-          Text("Username",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-          SizedBox(height:20),
-          Text(
-            username,
-            style: TextStyle(
-              color:Colors.blue,
-            ),
-          ),
-        ],
-      ),
+            const SizedBox(height:20),
+            ElevatedButton(onPressed: ()async{
+              await saveScore();
+            },
+             child: Text("Save Score"),
+             ),
+             SizedBox(height: 20),
+             ElevatedButton(onPressed: ()async{
+              await readScore();
+             }, child: Text("Read Score"),
+             ),
+             SizedBox(height:20),
+             Text(
+              "High Score",
+              style: TextStyle(
+                fontSize:22,
+                fontWeight: FontWeight.bold,
+              ),
+             ),
+             SizedBox(height:20),
+             Text(
+              "$highscore",
+              style: TextStyle(
+                fontSize: 30,
+                color:Colors.blue,
+              ),
+             ),
+          ],
+        ),
       ),
     );
   }
