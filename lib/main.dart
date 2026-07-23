0903e7b163f27fc93c5710aa1d75320fb47e8ea6
shahzaibwaheed522 <bilalzaibb12@gmail.com>
2026-07-23 14:12:09 +0500

    Provider Basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-void main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await Hive.openBox("mybox");
+import 'package:provider/provider.dart';
+void main(){
   runApp(
-    MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:HomeScreen(),
-    ));
+    ChangeNotifierProvider(create: (context)=>CounterProvider(),
+    child:  const MyApp(),
+    ),
+  );
 }
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({super.key});
-  State<HomeScreen> createState()=> _HomeScreenState();
+class CounterProvider extends ChangeNotifier{
+  int count=0;
+  void increment(){
+    count++;
+    notifyListeners();
+  }
 }
-class _HomeScreenState extends State<HomeScreen>{
-  TextEditingController namecontroller=TextEditingController();
-  var box=Hive.box("mybox");
-  String username="";
-  @override 
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override   
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatelessWidget{
+  HomeScreen({super.key});
+  @override   
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hive Example"),
-        centerTitle: true,
+        title: const Text("Provider Basics"),
       ),
-      body:Padding(     
-        padding: EdgeInsets.all(20),
+      body:Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: namecontroller,
-              decoration: InputDecoration(
-                hintText: "Enter Username",
-                border: OutlineInputBorder(),
+            Text(
+              "Counter",style: TextStyle(
+                fontSize:20,
               ),
             ),
-            const SizedBox(height:20),
-            ElevatedButton(onPressed: (){
-              box.put("username", namecontroller.text);
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Saved Successfully"),),
-              );
-            }, child: const Text("saved"),),
-            const SizedBox(height:20),
-            ElevatedButton(onPressed: (){
-              setState(() {
-                username=box.get("Username")?? "No data";
-              });
-            }, child: const Text("Read"),),
-            const SizedBox(height:20),
-            ElevatedButton(onPressed: (){
-              box.delete("Username");
-              setState(() {
-                username="Deleted";
-              });
-            }, child: const Text("delete"),),
-            const SizedBox(height:20),
-            Text(username,
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight:FontWeight.bold,
-            ),),
+            
+
+            Consumer<CounterProvider>(
+              builder: (context, provider, child) {
+                return Text(
+                  provider.count.toString(),
+                  style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+
+              onPressed: () {
+
+            
+                Provider.of<CounterProvider>(
+                  context,
+                  listen: false,
+                ).increment();
+
+              },
+
+              child: const Text("Increment"),
+
+            ),
+
           ],
         ),
       ),
     );
   }
 }
+
+    
